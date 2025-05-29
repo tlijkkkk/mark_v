57ae9378d28124edde11a1344e071ac5d656c943
Tiecong Li <tiecongli@Tiecongs-Mac-mini.local>
2025-05-29 02:15:16 -0700

    Update code

--- leetcode-practice/leetcode_practice/data_structure/stack/leetcode496_next_greater_element.py
+++ leetcode-practice/leetcode_practice/data_structure/stack/leetcode496_next_greater_element.py
@@ -3,12 +3,12 @@ from typing import List
 class Solution:
     def next_greater_element(self, nums1: List[int], nums2: List[int]) -> List[int]:
         dict = {}
-        stk = []
+        desc_mono_stk = []
         result = []
         for num in nums2:
-            while stk and stk[-1] < num:
-                dict[stk.pop()] = num
-            stk.append(num)
+            while desc_mono_stk and desc_mono_stk[-1] < num:
+                dict[desc_mono_stk.pop()] = num
+            desc_mono_stk.append(num)
             
         for num in nums1:
             result.append(dict.get(num, -1))
--- leetcode-practice/leetcode_practice/data_structure/stack/leetcode503_next_greater_element_2.py
+++ leetcode-practice/leetcode_practice/data_structure/stack/leetcode503_next_greater_element_2.py
@@ -5,21 +5,18 @@ from sortedcontainers import SortedDict
 
 class Solution:
     def next_greater_element_ii(self, nums: List[int]) -> List[int]:
-        desc_mono_stack = []
-        result = [0] * len(nums)
+        desc_mono_stk = []
+        result = [-1] * len(nums)
 
-        for i in range(2 * len(nums) - 1, -1, -1): # iterate backword
+        for i in range(len(nums) * 2):
             idx = i % len(nums)
-            while desc_mono_stack and nums[desc_mono_stack[-1]] <= nums[idx]:
-                desc_mono_stack.pop()
-
-            if not desc_mono_stack:
-                result[idx] = -1    
-            else:
-                result[idx] = nums[desc_mono_stack[-1]]
-            desc_mono_stack.append(idx)
+            while desc_mono_stk and nums[desc_mono_stk[-1]] < nums[idx]:
+                result[desc_mono_stk.pop()] = nums[idx]
+            desc_mono_stk.append(idx)
 
         return result
+
+        
             
     
         
